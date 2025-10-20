6ae412ae61c11adc663e6cb95cbbb4c05bb0f908
MAR miju <developerazizar@gmail.com>
2025-10-20 06:54:55 +0600

    if-elif-else

--- formating/formate.js
+++ formating/formate.js
@@ -1,10 +1,15 @@
 const mapping = [
   { from: /লিখো/g, to: "print" },
   { from: /যদি/g, to: "if" },
-  { from: /অথবা/g, to: "else" },
+  { from: /অথবা/g, to: "elif" },
+  { from: /নতুবাঃ/g, to: "else:" },
   { from: /যাবত/g, to: "while" },
   { from: /জন্য/g, to: "for"},
   { from: /পর্যন্ত/g, to: "in range" },
+  {from: /মধ্যে/g, to: "in"},
+  {from: /আইটেম/g, to: "items"},
+  {from: /।/g, to: "."},
+  {from: /মান/g, to:'values'},
   { from: /ঃ/g, to: ":" },
 ];
 
