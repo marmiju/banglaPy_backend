fb82d979d726d22179402b702defaa24da86614d
MAR_mju <developerazizar@gmail.com>
2025-11-09 12:02:32 +0600

    test authentication

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // your frontend
+    origin: 'http://localhost:3000', 
     credentials: true,
 }));
 
