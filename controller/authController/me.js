fcb02435fe482499a224ba4380eeba1b50053960
MAR_mju <developerazizar@gmail.com>
2025-11-09 17:52:58 +0600

    auth/me

--- controller/authController/me.js
+++ controller/authController/me.js
@@ -3,7 +3,6 @@ const GETME = (req, res) => {
   if (req.user) {
     return res.json(req.user)
   } else {
-   
     const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
     return res.redirect(FRONTEND_URL)  
   }
