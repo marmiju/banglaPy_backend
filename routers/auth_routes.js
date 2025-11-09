067a817352e75988573b42bafbcae913f4b5da18
MAR_mju <developerazizar@gmail.com>
2025-11-09 10:06:30 +0600

    authentication

---
+++ routers/auth_routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const passport = require('passport');
+const authRoute = express.Router();
+
+// Start Google login
+authRoute.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+// Google callback URL
+authRoute.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login',session:true}),
+  (req, res) => {
+    const FRONTEND_URL = process.env.FRONTEND_URL;
+
+    res.redirect(`${FRONTEND_URL}/profile`);
+ 
+  }
+);
+
+authRoute.get('/me', (req, res) => {
+  if (!req.user) return res.status(401).json({ message: 'Not logged in' });
+  res.json(req.user);
+});
+
+
+module.exports = authRoute;
--- server.js
+++ server.js
@@ -1,20 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 const { runBanglaCode } = require('./controller/BanglaToPython');
+require('./config/passportSetUp')
+const authRoute = require('./routers/auth_routes')
+const passport = require('passport')
+const session = require('express-session')
+
+// configuration
 require('dotenv').config();
 
 const port = process.env.PORT || 5000
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // your frontend
+    credentials: true,
+}));
 
 
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || '1234',
+        resave: false,             
+        saveUninitialized: false,  
+        cookie: {
+            maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+            httpOnly: true,              // prevents client-side JS access
+            secure: false,              
+        },
+    })
+);
+
+// initialize passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+// handlimg routes
 app.get('/', (req, res) => {
     res.send('App is running!');
 });
 
-app.post('/toPython',runBanglaCode)
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.use('/auth', authRoute)
+
+app.post('/toPython', runBanglaCode)
 
 app.listen(port, () => {
     console.log(`App running on port localhost:${port}`);
