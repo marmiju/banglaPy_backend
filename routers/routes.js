4406bb38714cd888b83336b19495e149c8eca397
MAR_mju <developerazizar@gmail.com>
2025-11-12 22:55:53 +0600

    cokkie policy

--- routers/routes.js
+++ routers/routes.js
@@ -8,11 +8,11 @@ const router = express.Router()
 
 // code run and submition routes
 router.post('/runcode',RunCode )
-router.post('/submitcode',submitSolution )
+router.post('/submitcode',isLogedIn,submitSolution )
 
 router.post('/Problem',createProblem ) //create new problem
-router.get('/problems',isLogedIn, getProblems )         // get  problems
-router.get('/problem/:id',isLogedIn, getProblem )       // get  problem
+router.get('/problems', getProblems )         // get  problems
+router.get('/problem/:id', getProblem )       // get  problem
 router.delete('/problem/:id',deletProblem )  //delete problem
 router.patch('/problem/:id',UpdateProblem )  //update problem
 
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   },
 }));
 
