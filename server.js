8063141dd480b50e338febdab618456d825f3bc8
MAR_mju <developerazizar@gmail.com>
2025-11-14 05:20:34 +0600

    secure -0

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.use(session({
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
-    
+    secure: false,
+
     // secure: process.env.NODE_ENV === 'production',
     // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   },
